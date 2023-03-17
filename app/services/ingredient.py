8cdb761e89856314cadf3518db70f3c21aeca031
HatusP25 <83603246+HatusP25@users.noreply.github.com>
2023-03-17 11:46:20 -0500

    fix: Implement template method pattern (#8)

--- app/services/ingredient.py
+++ app/services/ingredient.py
@@ -1,28 +1,5 @@
-from app.common.http_methods import GET, POST, PUT
-from flask import Blueprint, jsonify, request
-
-from .BaseService import BaseService
+from .BaseBlueprint import BaseBlueprint
 from ..controllers import IngredientController
 
 
-ingredient = Blueprint('ingredient', __name__)
-service = BaseService(IngredientController)
-
-
-@ingredient.route('/', methods=POST)
-def create_ingredient():
-    return service.create()
-
-@ingredient.route('/', methods=PUT)
-def update_ingredient():
-    return service.update()
-
-
-@ingredient.route('/id/<_id>', methods=GET)
-def get_ingredient_by_id(_id: int):
-    return service.get_by_id(_id)
-
-
-@ingredient.route('/', methods=GET)
-def get_ingredients():
-    return service.get_all()
+ingredient = BaseBlueprint('ingredient', __name__, IngredientController)
